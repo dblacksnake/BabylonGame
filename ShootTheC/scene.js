9842b558da73db443f1be3159ff4211455160b9a
dblacksnake <63682911+dblacksnake@users.noreply.github.com>
2021-11-13 00:14:14 -0500

    Update

--- ShootTheC/scene.js
+++ ShootTheC/scene.js
@@ -4,27 +4,35 @@ var engine = new BABYLON.Engine(canvas,true)
 
 
 //variables
-let scene,light,player,floor = null
+let scene,light,player,floor,poX,poY,poZ,angleX,angleY,angleZ,mesh = null
 var isGravity = true
 var force = null
 
 
 window.addEventListener('click',(event) =>{
-    console.log(player.position)
-    console.log(player.rotation)
-    console.log(event)
+    console.log("playey position" + player.position)
+    console.log("playey rotation" +player.rotation.z)
+    console.log("event" + event.position)
+
+    let v0 = new BABYLON.Vector3(0, 0, 1);
+    let v1 = 5;
+    
+    //v1.normalize();
+    
+    let angle = Math.acos(BABYLON.Vector3.Dot(v0, v1));
+    let angleInDegree = BABYLON.Tools.ToDegrees(angle)
+    console.log("angleInDegree" + angleInDegree)
 
     var projectile = new BABYLON.MeshBuilder.CreateSphere('projectiles',{})
     var projMat = new BABYLON.StandardMaterial('material',scene)
-    var angleX =Math.atan(event.clientX - player.position.x)
-    var angleZ =Math.atan(event.clientY - player.position.z)
-    var poX = Math.cos(angleX) * (player.rotation.x +player.position.x)
+   
 
     projMat.diffuseColor = new BABYLON.Color3(1,0,0)
     projectile.material = projMat
-    projectile.position = new BABYLON.Vector3(player.rotation.x * -angleX,player.position.y, Math.atan2(player.position.y,player.position.x))
+    projectile.position = new BABYLON.Vector3(angleX - (angleX - player.position.x) + 1,angleY * (player.rotation.y - player.position.y),angleY + angleX)
     //var projectile = new Projectiles(0,10,0,{mat1 = 1,mat2 = 0,mat3 = 0},'red',scene)
-    console.log(projectile.position)
+    console.log("Projectile position" + projectile.position)
+    console.log("X in degrees" + BABYLON.Tools.ToDegrees(player.rotation.x))
 },false)
 
 //Pointer lock on click !! 
@@ -42,6 +50,9 @@ let createPointerLock = function(scene) {
 var createScene = function() {
     scene = new BABYLON.Scene(engine)
     scene.clearColor = new BABYLON.Color3.Black();
+
+
+
 
     var floor = new BABYLON.Mesh.CreatePlane('floor',100, scene)
     var material = new BABYLON.StandardMaterial('material', scene)
@@ -95,11 +106,17 @@ function Player(){
     // } 
 }
 
-
 //Call Method
 createScene();
 
 engine.runRenderLoop(() =>{
+    angleX =BABYLON.Tools.ToDegrees(player.rotation.y)
+    angleZ =BABYLON.Tools.ToDegrees(player.rotation.x * player.rotation.y)
+    angleY =BABYLON.Tools.ToDegrees(player.rotation.x)
+
+    poX = Math.cos(angleX) * (player.position.x)
+    poZ = Math.cos(angleZ) * (player.position.z)
+    poY = Math.cos(angleY) * (player.position.y)
     if(player.rotation.x >= -0.001){
         player.rotation.x == 0
     }
