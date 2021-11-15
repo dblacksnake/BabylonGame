32d24ebfe71909176200dcb0a440e5d95df3b8a0
dblacksnake <63682911+dblacksnake@users.noreply.github.com>
2021-11-15 00:36:38 -0500

    camera getTarget update

--- ShootTheC/scene.js
+++ ShootTheC/scene.js
@@ -5,34 +5,18 @@ var engine = new BABYLON.Engine(canvas,true)
 
 //variables
 let scene,light,player,floor,poX,poY,poZ,angleX,angleY,angleZ,mesh = null
-var isGravity = true
 var force = null
 
-
 window.addEventListener('click',(event) =>{
-    console.log("playey position" + player.position)
-    console.log("playey rotation" +player.rotation.z)
-    console.log("event" + event.position)
-
-    let v0 = new BABYLON.Vector3(0, 0, 1);
-    let v1 = 5;
-    
-    //v1.normalize();
-    
-    let angle = Math.acos(BABYLON.Vector3.Dot(v0, v1));
-    let angleInDegree = BABYLON.Tools.ToDegrees(angle)
-    console.log("angleInDegree" + angleInDegree)
-
+       
+    //Projectiles Creation
     var projectile = new BABYLON.MeshBuilder.CreateSphere('projectiles',{})
     var projMat = new BABYLON.StandardMaterial('material',scene)
-   
-
     projMat.diffuseColor = new BABYLON.Color3(1,0,0)
     projectile.material = projMat
-    projectile.position = new BABYLON.Vector3(angleX - (angleX - player.position.x) + 1,angleY * (player.rotation.y - player.position.y),angleY + angleX)
-    //var projectile = new Projectiles(0,10,0,{mat1 = 1,mat2 = 0,mat3 = 0},'red',scene)
-    console.log("Projectile position" + projectile.position)
-    console.log("X in degrees" + BABYLON.Tools.ToDegrees(player.rotation.x))
+    projectile.applyGravity= true
+    projectile.position = new BABYLON.Vector3(player.getTarget().x, player.getTarget().y, player.getTarget().z)
+
 },false)
 
 //Pointer lock on click !! 
@@ -49,8 +33,10 @@ let createPointerLock = function(scene) {
 
 var createScene = function() {
     scene = new BABYLON.Scene(engine)
+    
+    scene.collisionsEnabled = true
     scene.clearColor = new BABYLON.Color3.Black();
-
+    console.log(scene.Vector3)
 
 
 
@@ -58,6 +44,7 @@ var createScene = function() {
     var material = new BABYLON.StandardMaterial('material', scene)
     material.diffuseColor = new BABYLON.Color3.White();
     floor.material = material;
+    floor.checkCollisions = true;
     floor.rotation = new BABYLON.Vector3(1.5,0,0)
     console.log(floor.position)
     floor.position.x = 0
@@ -89,11 +76,10 @@ function Lights() {
 
 function Player(){
     player = new BABYLON.FreeCamera('player',new BABYLON.Vector3(10,10,-20),scene,true);
-
-    console.log(player.position);
-    // console.log(window.mouse.y);
     player.attachControl(canvas,false)
-    player.applyGravity = isGravity;
+   //player.applyGravity = true;
+   // player.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+    player.checkCollisions = true;
     player.keysUp.push(87)
     player.keysDown.push(83)
     player.keysLeft.push(65)
@@ -110,23 +96,17 @@ function Player(){
 createScene();
 
 engine.runRenderLoop(() =>{
-    angleX =BABYLON.Tools.ToDegrees(player.rotation.y)
-    angleZ =BABYLON.Tools.ToDegrees(player.rotation.x * player.rotation.y)
-    angleY =BABYLON.Tools.ToDegrees(player.rotation.x)
+    scene.gravity = new BABYLON.Vector3(0,-10,0)
 
-    poX = Math.cos(angleX) * (player.position.x)
-    poZ = Math.cos(angleZ) * (player.position.z)
-    poY = Math.cos(angleY) * (player.position.y)
-    if(player.rotation.x >= -0.001){
-        player.rotation.x == 0
-    }
+
+    // if(player.rotation.x >= -0.001){
+    //     player.rotation.x == 0
+    // }
     if(player.position.y  <= 1){
         force =0
     }else{
         force = 0.05;
         player.position.y -= force
     }
-    //console.log(player.position)
-    // console.log(player.rotation)
-    scene.render();
+     scene.render();
 })
